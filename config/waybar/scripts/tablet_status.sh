c0188ac05b039bc845443f7c24d8af4684d5a884
suyash <yashcube07@gmail.com>
2026-08-18 16:27:57 +0530

    Fix JSON escaping in waybar tablet_status script

--- config/waybar/scripts/tablet_status.sh
+++ config/waybar/scripts/tablet_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Check if wayvnc or HEADLESS monitor is active
 if pgrep -x "wayvnc" > /dev/null || hyprctl monitors | grep -q "HEADLESS"; then
-    printf '{"text": "󰢹 Tablet ON", "class": "connected", "tooltip": "Tablet 2nd Screen: ACTIVE (Left 1.5x)\n👉 Click to STOP / Disconnect"}\n'
+    echo '{"text":"󰢹 Tablet ON","class":"connected","tooltip":"Tablet 2nd Screen: ACTIVE\\nClick to STOP"}'
 else
-    printf '{"text": "󰢹 Tablet OFF", "class": "disconnected", "tooltip": "Tablet 2nd Screen: OFF\n👉 Click to START / Connect"}\n'
+    echo '{"text":"󰢹 Tablet OFF","class":"disconnected","tooltip":"Tablet 2nd Screen: OFF\\nClick to START"}'
 fi
